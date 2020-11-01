b5788486b24ce4399875297c0b975200fff93630
GaladonZ <67939981+ArefinAkash@users.noreply.github.com>
2020-11-01 13:11:12 +0600

    Added all files from my local pc

---
+++ assets/js/scripts.js
@@ -0,0 +1,144 @@
+/*
+* ----------------------------------------------------------------------------------------
+Author       : wordpressboss
+Template Name: Brohost - One Page Responsive Hosting Template
+Version      : 1.0                                          
+* ----------------------------------------------------------------------------------------
+*/
+
+
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PRELOADER JS
+         * ----------------------------------------------------------------------------------------
+         */
+        var prealoaderOption = $(window);
+        prealoaderOption.on("load", function () {
+            var preloader = jQuery('#loader');
+            var preloaderArea = jQuery('.preloader-area');
+            preloader.fadeOut();
+            preloaderArea.delay(350).fadeOut('slow');
+        });
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  CHANGE MENU BACKGROUND JS
+         * ----------------------------------------------------------------------------------------
+         */
+        var headertopoption = $(window);
+        var headTop = $('.header-top-area');
+
+        headertopoption.on('scroll', function () {
+            if (headertopoption.scrollTop() > 200) {
+                headTop.addClass('menu-bg');
+            } else {
+                headTop.removeClass('menu-bg');
+            }
+        });
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SMOTH SCROOL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  TESTIMONIAL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(".testimonial-list").owlCarousel({
+            items: 3,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: true,
+            autoHeight: true,
+        });
+
+
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  EXTRA JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SCROOL TO UP JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(window).on("scroll", function () {
+            if ($(this).scrollTop() > 250) {
+                $('.scrollup').fadeIn();
+            } else {
+                $('.scrollup').fadeOut();
+            }
+        });
+        $('.scrollup').on("click", function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  WOW JS
+         * ----------------------------------------------------------------------------------------
+         */
+        new WOW().init();
+
+
+
+
+
+
+
+    });
+
+})(jQuery);
